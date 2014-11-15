9973ec3291f5a45c231bf3e0bfe6aba0376cc2c3
Josh Cavin <josh@dreamingblue.com>
2014-11-15 13:45:56 -0800

    added description to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115212605) do
+ActiveRecord::Schema.define(version: 20141115214438) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20141115212605) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.text     "description"
   end
 
   create_table "reviews", force: true do |t|
