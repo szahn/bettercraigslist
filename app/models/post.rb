646671fd79d32b962536cd306bf67a8204e0bc3a
Josh Cavin <josh@dreamingblue.com>
2014-11-15 15:28:25 -0800

    updates to home page

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ActiveRecord::Base
 	has_many :comments
 
 	has_attached_file :image, :styles => { :medium => "574x323#", :threeacross => "390x219#", :thumb => "100x56#" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,14 @@ Rails.application.configure do
 
   #devise 
   config.action_mailer.default_url_options = { :host => 'https://bettercraigslist.herokuapp.com/' }
+
+  #amazon paperclip 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
