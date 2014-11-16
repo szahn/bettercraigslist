a9b2fb7aa2bb74705e250f0e90cc9df778cdb4bc
Josh Cavin <josh@dreamingblue.com>
2014-11-16 13:16:06 -0800

    setup the profile page to work but need to styoe

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	belongs_to :category
 
+	validates_presence_of :title, :category_id, :image, :description, :price
 	has_attached_file :image, :styles => { :medium => "574x323#", :threeacross => "390x219#", :thumb => "100x56#" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	#do_not_validate_attachment_file_type :image
