6b374d5ff54b18c2fba8dbfff5be46be3739dc65
szahn <stuart.zahn@gmail.com>
2014-11-15 20:18:02 -0800

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,5 @@ Rails.application.routes.draw do
   resources :users, :only => [:show]
   
   root :to => 'pages#home'
-  get "sports" => "pages#sports"
-  get "cat/:category" => "categories#show"
 
 end
